216b346896dd3d199743ddfa00d57a11290a7a97
thomascsd <thomascsd808@gmail.com>
2020-06-20 16:09:07 +0800

    add dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { Component,  OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgxSmartModalService } from "ngx-smart-modal";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent implements OnInit  {
-  
-  constructor(){}
+export class AppComponent implements OnInit {
+  constructor(private modalService: NgxSmartModalService) {}
 
-  ngOnInit(){
+  ngOnInit() {}
 
+  openDialog() {
+    this.modalService.getModal("dialog1").open();
+  }
+
+  openDialogLayer1() {
+    this.modalService.getModal("dialog2").open();
+  }
+  openDialogLayer2() {
+    this.modalService.getModal("dialog3").open();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { NgxSmartModalModule } from 'ngx-smart-modal';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { NgxSmartModalModule } from "ngx-smart-modal";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, NgxSmartModalModule.forRoot() ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, NgxSmartModalModule.forRoot()],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
